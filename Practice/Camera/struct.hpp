4fc68a6bb73c6b2f1b7a1eedc7bb6a30644f364e
Ocean-Eyes <68937084+Ocean-Eyes@users.noreply.github.com>
2023-08-13 16:02:54 +0900

    Learn camera calibration and visual odometry

---
+++ Practice/Camera/struct.hpp
@@ -0,0 +1,8 @@
+#ifndef STRUCT_HPP
+#define STRUCT_HPP
+
+struct LidarPoint { // single lidar point in space
+    double x, y, z, r; // x,y,z in [m], r is point reflectivity
+};
+
+#endif // STRUCT_HPP
---
+++ Practice/Camera/utils.hpp
@@ -0,0 +1,15 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <stdio.h>
+
+#include "struct.hpp"
+
+void writeLidarPts(std::vector<LidarPoint>& input, const char* fileName);
+void readLidarPts(const char* fileName, std::vector<LidarPoint>& output);
+template<typename T> void write_pod(std::ofstream& out, T& t);
+template<typename T> void read_pod(std::ifstream& in, T& t);
+template<typename T> void read_pod_vector(std::ifstream& in, std::vector<T>& vect);
+template<typename T> void write_pod_vector(std::ofstream& out, std::vector<T>& vect);
+
+#endif // UTILS_HPP
